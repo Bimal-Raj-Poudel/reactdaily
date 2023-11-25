1fc4a7ccf14ff0ba52b33c1f89229941190ee83e
Bimal Raj Poudel <startertechz@gmail.com>
2023-11-25 10:56:40 +0545

    nov 25

---
+++ routerproject/vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css'  
+import Home from './components/Home/Home'
+// import Header from './components/Header/Header'
+// import About from './components/About/Aboutus'
+// import Contact from './components/contact/Contact'
+// import Footer from './components/Footer/Footer'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Home/>
+   {/* <Header/>
+   <About/>
+   <Contact/>
+   <Footer/> */}
+    </>
+  )
+}
+
+export default App
